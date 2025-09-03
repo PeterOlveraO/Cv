2eeea65e905a8263d8b807f4f973e40548be9e59
Petter <163795670+PeterOlveraO@users.noreply.github.com>
2025-09-03 00:58:56 -0600

    Cv acabado

---
+++ src/data/cvData.js
@@ -0,0 +1,66 @@
+// Datos personales para el CV
+export const personalData = {
+  nombre: "Pedro Alberto Olvera Ochoa",
+  titulo: "Ingeniero en sistemas computacionales",
+  fotoPerfil: "/imagenes/foto-perfil.webp", // Ruta a tu foto de perfil en formato WebP
+  descripcionPersonal: "Futuro profesional de DevOps con enfoque en la integración de la estabilidad y la innovación. Mi objetivo es construir sistemas robustos que permitan a los equipos de desarrollo innovar de manera ágil y segura, asegurando un crecimiento continuo.",
+  contacto: {
+    email: "pedro.olvera.8a@gmail.com",
+    telefono: "+52 833 100 7920",
+    linkedin: "www.linkedin.com/in/pedro-olvera-764584298",
+    ubicacion: "Cd. Madero, Tamaulipas, México"
+  }
+};
+
+// Datos de formación académica
+export const formacionAcademica = [
+  {
+    titulo: "Ingeniero en Sistemas Computacionales",
+    institucion: "Universidad Autonoma de Tamaulipas",
+    ubicacion: "Cd. Madero, Tamaulipas, México",
+    periodo: "2023 - 2027",
+    
+  },
+  {
+    titulo: "Tecnico Programador",
+    institucion: "Cetis 109",
+    ubicacion: "Cd. Madero, Tamaulipas, México",
+    periodo: "2020 - 2023",
+    
+  }
+];
+
+// Datos de experiencia profesional
+export const experienciaProfesional = [
+  {
+    puesto: "[Ayudante de chef]",
+    empresa: "[Querido ramen]",
+    ubicacion: "Cd. Madero, Tamaulipas, México",
+    periodo: "Julio 2025 - Presente",
+    responsabilidades: [
+      "Asistir en la preparación y presentación de platos",
+      "Mantener la limpieza y organización de la cocina"
+    ]
+  },
+  {
+    puesto: "[Asistente de Ingeniero en sistemas]",
+    empresa: "[Chevrolet AutoIdeal Matriz]",
+    ubicacion: "Tampico, Tamaulipas, México",
+    periodo: "Agosto 2022 - Mayo 2023",
+    responsabilidades: [
+      "Asistir en la resolución de problemas técnicos",
+      "Realizar pruebas y depuración de sistemas",
+      "Documentar procesos y procedimientos"
+    ]
+  }
+];
+
+// Datos de habilidades
+export const habilidades = {
+  tecnicas: ["Python", "Sql", "Java", "Linux", "Supabase"],
+  idiomas: [
+    { idioma: "Español", nivel: "Nativo" },
+    { idioma: "Inglés", nivel: "Basico (A1)" }
+  ],
+  blandas: ["Liderazgo", "Comunicación efectiva", "Resolución de problemas", "Trabajo en equipo", "Adaptabilidad"]
+};
